41cb866abe79d4916bb6b756550883ba44f31124
Felipe Ribeiro <felipernb@gmail.com>
2012-08-30 16:44:29 +0200

    Quicksort in Go

---
+++ go/quicksort.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Quicksort(a []int) []int {
+	if (len(a) <= 1) {
+		return a
+	}
+	p := partition(a)
+	go Quicksort(a[0:p])
+	go Quicksort(a[p+1:])
+	return a
+}
+
+func partition(a []int) int {
+	pivot := a[0]
+	i := 1
+	for j := i; j < len(a); j++ {
+		if a[j] < pivot {
+			a[j], a[i] = a[i], a[j]
+			i++
+		}
+	}
+	a[0], a[i-1] = a[i-1], a[0]
+	return i-1
+}
+
+func main() {
+	a := []int {5,1,0,2,4,-1}
+	fmt.Println(Quicksort(a))
+}
