242304d35a2005c44b3312f111c9e34e831a1f25
Felipe Ribeiro <felipernb@gmail.com>
2012-09-14 10:51:03 +0200

    Tabs vs. spaces in insertion_sort.rb

--- rb/insertion_sort.rb
+++ rb/insertion_sort.rb
@@ -1,12 +1,12 @@
 def insertion_sort(a)
-	for i in 1..a.length-1 do
-		n = a[i]
-		pos = i
-		while pos > 0 and a[pos-1] > n do
-			a[pos] = a[pos-1]
-			pos -= 1
-		end
-		a[pos] = n
-	end
-	a
+    for i in 1..a.length-1 do
+        n = a[i]
+        pos = i
+        while pos > 0 and a[pos-1] > n do
+            a[pos] = a[pos-1]
+            pos -= 1
+        end
+        a[pos] = n
+    end
+    a
 end
